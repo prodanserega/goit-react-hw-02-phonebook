5ae53ed06742df3e6e940cab39e8c9793c8fb6e0
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-07-21 17:36:17 +0300

    Form

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,44 @@
 import { Component } from "react";
-import React from "react";
 
-class App extends Component {
+import ContactForm from ".//ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  handelAddContact = (newContact) =>
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, newContact],
+    }));
+
+  handelCheckUniqueContact = (name) => {
+    const { contacts } = this.state;
+
+    const isExistContact = !!contacts.find(
+      (contacts) => contacts.name === name
+    );
+    isExistContact && alert("Contact is already exist");
+
+    return !isExistContact;
+  };
+
+  handleRemoveContact = (id) =>
+    this.state(({ contacts }) => ({
+      contacts: contacts.filter((contact) => contact.id !== id),
+    }));
+
   render() {
-    return <div></div>;
+    const { contacts } = this.state;
+    return (
+      <>
+        <ContactForm
+          onAdd={this.handelAddContact}
+          onCheckUnique={this.handelCheckUniqueContact}
+        />
+        <ContactList contacts={contacts} onRemove={this.handleRemoveContact} />
+      </>
+    );
   }
 }
-
-export default App;
